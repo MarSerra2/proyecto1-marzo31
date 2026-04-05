25754b9bf818fb25dfd1f22ef9d6dba636eda6a2
Maria del Mar Serra <mserra1092.2@gmail.com>
2026-04-05 04:04:06 -0300

    Primera prueba con sensor DHT11

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
 #include <Arduino.h>
+#include <DHTesp.h>
 
 const byte led_gpio = 32;
+const byte dht_gpio = 4;
+DHTesp dht;
+
+void estadoLed(bool estado) {
+  digitalWrite(led_gpio, estado);
+  delay(1000);
+}
+
 void setup() {
+  Serial.begin(115200);
   pinMode(led_gpio, OUTPUT);
+  dht.setup(dht_gpio, DHTesp::DHT11);
 }
 void loop() {
-  digitalWrite(led_gpio, HIGH);
-  delay(1000);
-  digitalWrite(led_gpio, LOW);
-  delay(1000);
+  TempAndHumidity data = dht.getTempAndHumidity();
+  if (dht.getStatus() == DHTesp::ERROR_NONE) {
+    Serial.print("Temperature: ");
+    Serial.print(data.temperature);
+    Serial.print("°C, Humidity: ");
+    Serial.print(data.humidity);
+    Serial.println("%");
+  } else {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+
+  estadoLed(HIGH);
+  estadoLed(LOW);
 }
