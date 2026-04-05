a739029c79aa23d73cab4c5173f2a702cd801999
Maria del Mar Serra <mserra1092.2@gmail.com>
2026-04-05 05:19:20 -0300

    Primer servidor web de visualizacion de datos con el sensor DHT11

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,47 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h>
 #include <DHTesp.h>
 
-const byte led_gpio = 32;
 const byte dht_gpio = 4;
 DHTesp dht;
 
-void estadoLed(bool estado) {
-  digitalWrite(led_gpio, estado);
-  delay(1000);
-}
+AsyncWebServer server(80);
+DNSServer dns;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(led_gpio, OUTPUT);
-  dht.setup(dht_gpio, DHTesp::DHT11);
-}
-void loop() {
-  TempAndHumidity data = dht.getTempAndHumidity();
-  if (dht.getStatus() == DHTesp::ERROR_NONE) {
-    Serial.print("Temperature: ");
-    Serial.print(data.temperature);
-    Serial.print("°C, Humidity: ");
-    Serial.print(data.humidity);
-    Serial.println("%");
-  } else {
-    Serial.println("Failed to read from DHT sensor!");
-  }
 
-  estadoLed(HIGH);
-  estadoLed(LOW);
+  dht.setup(dht_gpio, DHTesp::DHT11);
+
+  AsyncWiFiManager wifiManager(&server, &dns);
+
+  Serial.println("Conectando al WiFi...");
+  wifiManager.autoConnect("ESP32_Config");
+
+  Serial.println("¡Conectado al WiFi con éxito!");
+  Serial.print("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    TempAndHumidity data = dht.getTempAndHumidity();
+    
+    String html = "<h1>Monitor de Clima ESP32</h1>";
+    
+    if (dht.getStatus() == DHTesp::ERROR_NONE) {
+      html += "<p>Temperatura: " + String(data.temperature) + " C</p>";
+      html += "<p>Humedad: " + String(data.humidity) + " %</p>";
+    } else {
+      html += "<p>Error leyendo el sensor DHT11</p>";
+    }
+    
+    request->send(200, "text/html", html);
+  });
+
+  server.begin();
+}
+
+void loop() {
+  
 }
